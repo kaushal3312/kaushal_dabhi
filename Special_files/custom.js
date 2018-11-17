e7bc15ea3e06db13185ce9fe9239b74262ba6fcf
Kaushal <kaushalmdabhi@gmail.com>
2018-11-17 12:11:51 +0530

    17Nov.2018

--- Special_files/custom.js
+++ Special_files/custom.js
@@ -1,3 +1,51 @@
+
+var loadSpace = document.getElementById('loader');
+var mainLoop = document.createElement('div');
+var shadeDot = document.createElement('div');
+var arcaPipe = document.createElement('div');
+var loadBars = document.createElement('div');
+var loadNumb = document.createElement('div');
+
+mainLoop.className = "dotring";
+shadeDot.className = "spheric";
+arcaPipe.className = "pipline";
+loadBars.className = "loadbar";
+loadNumb.className = "numbers";
+
+function setPack(num){
+  loadBars.innerHTML = '';
+  for (i=0;i<num;i++){
+    var loadPack = document.createElement('div');
+    loadPack.className = "loadpac";
+    loadPack.style.zIndex = 100 - i;
+    loadPack.style.backgroundPosition = "0 "+(i*30)+"px";
+    loadBars.appendChild(loadPack);
+  }
+}
+
+mainLoop.appendChild(shadeDot);
+mainLoop.appendChild(arcaPipe);
+loadSpace.appendChild(mainLoop);
+loadSpace.appendChild(loadBars);
+
+var prog = 0;
+function loadry(){
+  prog ++;
+  if(prog <= 100){
+    boxn = Math.floor(prog/5.5);
+    setPack(boxn);
+    loadNumb.innerHTML = prog;
+    loadSpace.appendChild(loadNumb);
+  } else if (prog == 120){
+    loadSpace.style.opacity = 0;
+  } else if (prog >= 130){
+    loadSpace.style.opacity = 1;
+    prog = 0;
+  };
+  setTimeout(loadry,(Math.random()*120)+40);
+}
+loadry();
+
 $(function() {
     "use strict";
     var wind = $(window);
